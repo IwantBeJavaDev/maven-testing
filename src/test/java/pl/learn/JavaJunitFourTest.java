9b0478115e0ccd1b1b8419767a4ed90886cf867f
iwantbejavadev <m.baraniak@streamsoft.pl>
2019-04-03 14:50:31 +0200

    Junit4and5 maven

---
+++ src/test/java/pl/learn/JavaJunitFourTest.java
@@ -0,0 +1,10 @@
+package pl.learn;
+
+import org.junit.Test;
+
+public class JavaJunitFourTest {
+    @Test
+    public void someTestForJunit4() {
+        System.out.println("Junit 4 test");
+    }
+}
