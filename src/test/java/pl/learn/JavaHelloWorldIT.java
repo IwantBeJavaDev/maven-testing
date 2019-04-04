8eda76e1dada327003581293400d6cdedff73d11
iwantbejavadev <m.baraniak@streamsoft.pl>
2019-04-04 08:56:04 +0200

    Add integration test. failsafe

---
+++ src/test/java/pl/learn/JavaHelloWorldIT.java
@@ -0,0 +1,10 @@
+package pl.learn;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+    @Test
+    void myFunctionIntegrationTest() {
+        System.out.println("Integration test");
+    }
+}
